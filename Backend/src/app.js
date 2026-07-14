faced6a4073ecd41a888c372fec468c1bfbab448
Uday Yelve <udayyelve7777@gmail.com>
2026-07-14 16:35:29 +0530

    cors setting changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['https://forever-clothing-six.vercel.app', 'http://localhost:5174'],
   credentials:true
 }))
 app.use(express.urlencoded({ extended: true }))
